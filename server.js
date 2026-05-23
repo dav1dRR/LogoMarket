5f868266adf9f2cff5984b271a8775353de6838d
Jonathan <jorodriguez52122@gmail.com>
2026-05-22 21:16:18 -0500

    Primer commit

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const mysql = require("mysql2");
+const path = require("path");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname)));
+
+const conexion = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "LogoMARKET1"
+});
+
+conexion.connect((error) => {
+
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Conexión exitosa");
+    }
+
+});
+
+app.post("/registro", (req, res) => {
+
+    const nombre = req.body.nombre;
+    const correo = req.body.correo;
+    const password = req.body.password;
+
+    const sql = `
+    INSERT INTO usuarios(nombre, correo, password)
+    VALUES (?, ?, ?)
+    `;
+
+    conexion.query(sql,
+    [nombre, correo, password],
+    (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        }else{
+            res.send("Usuario registrado");
+        }
+
+    });
+
+});
+
+app.post("/login", (req, res) => {
+
+    const correo = req.body.correo;
+    const password = req.body.password;
+
+    const sql = `
+    SELECT * FROM usuarios
+    WHERE correo = ? AND password = ?
+    `;
+
+    conexion.query(sql,
+    [correo, password],
+    (error, resultado) => {
+
+        if(error){
+            console.log(error);
+        }else{
+
+            if(resultado.length > 0){
+                res.send("Login correcto");
+            }else{
+                res.send("Usuario incorrecto");
+            }
+
+        }
+
+    });
+
+});
+
+app.listen(3000, () => {
+    console.log("Servidor funcionando");
+});
